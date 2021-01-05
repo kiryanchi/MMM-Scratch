35ed945ee08655024f9ca522a6ae52c71fc1b7dc
kiryanchi <kiryanchi@naver.com>
2021-01-05 18:56:28 +0900

    start function -ing

--- MMM-Scratch.js
+++ MMM-Scratch.js
@@ -17,14 +17,12 @@ Module.register("MMM-Scratch", {
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	getEmbed: function() {
-		const projectUrl = this.config.project;
-
-		return projectUrl + '/embed';
-	},
-
 	start: function() {
 		Log.info(`Starting module: ` + this.name);
+
+		const projectEmbed = getEmbed();
+
+
 		// var self = this;
 		// var dataRequest = null;
 		// var dataNotification = null;
@@ -39,6 +37,106 @@ Module.register("MMM-Scratch", {
 		// }, this.config.updateInterval);
 	},
 
+	getEmbed: function() {
+		const projectUrl = this.config.project;
+
+		return projectUrl + '/embed';
+	},
+
+
+	
+	
+	/* scheduleUpdate()
+	* Schedule next update.
+	*
+	* argument delay number - Milliseconds before next update.
+	*  If empty, this.config.updateInterval is used.
+	*/
+	scheduleUpdate: function(delay) {
+		var nextLoad = this.config.updateInterval;
+		if (typeof delay !== "undefined" && delay >= 0) {
+			nextLoad = delay;
+		}
+		nextLoad = nextLoad ;
+		var self = this;
+		setTimeout(function() {
+			self.getData();
+		}, nextLoad);
+	},
+	
+	getDom: function() {
+		var self = this;
+		
+		// create element wrapper for show into the module
+		const wrapper = document.createElement("div");
+		wrapper.className = 'scratch';
+	// If this.dataRequest is not empty
+		if (this.dataRequest) {
+			var wrapperDataRequest = document.createElement("div");
+			// check format https://jsonplaceholder.typicode.com/posts/1
+			wrapperDataRequest.innerHTML = this.dataRequest.title;
+			
+			var labelDataRequest = document.createElement("label");
+			// Use translate function
+			//             this id defined in translations files
+			labelDataRequest.innerHTML = this.translate("TITLE");
+			
+			
+			wrapper.appendChild(labelDataRequest);
+			wrapper.appendChild(wrapperDataRequest);
+		}
+		
+		// Data from helper
+		if (this.dataNotification) {
+			var wrapperDataNotification = document.createElement("div");
+			// translations  + datanotification
+			wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
+
+			wrapper.appendChild(wrapperDataNotification);
+		}
+		return wrapper;
+	},
+	
+	getScripts: function() {
+		return [];
+	},
+	
+	getStyles: function () {
+		return [
+			"MMM-Scratch.css",
+		];
+	},
+	
+	// Load translations files
+	getTranslations: function() {
+		//FIXME: This can be load a one file javascript definition
+		return {
+			en: "translations/en.json",
+			es: "translations/es.json",
+			ko: "translations/ko.json"
+		};
+	},
+	
+	processData: function(data) {
+		var self = this;
+		this.dataRequest = data;
+		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
+		this.loaded = true;
+		
+		// the data if load
+		// send notification to helper
+		this.sendSocketNotification("MMM-Scratch-NOTIFICATION_TEST", data);
+	},
+	
+	// socketNotificationReceived from helper
+	socketNotificationReceived: function (notification, payload) {
+		if(notification === "MMM-Scratch-NOTIFICATION_TEST") {
+			// set dataNotification
+			this.dataNotification = payload;
+			this.updateDom();
+		}
+	},
+
 	/*
 	 * getData
 	 * function example return data and show it in the module wrapper
@@ -47,10 +145,10 @@ Module.register("MMM-Scratch", {
 	 */
 	getData: function() {
 		var self = this;
-
+	
 		var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
 		var retry = true;
-
+	
 		var dataRequest = new XMLHttpRequest();
 		dataRequest.open("GET", urlApi, true);
 		dataRequest.onreadystatechange = function() {
@@ -72,96 +170,5 @@ Module.register("MMM-Scratch", {
 			}
 		};
 		dataRequest.send();
-	},
-
-
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
-
-	getDom: function() {
-		var self = this;
-
-		// create element wrapper for show into the module
-		var wrapper = document.createElement("div");
-		// If this.dataRequest is not empty
-		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
-			// check format https://jsonplaceholder.typicode.com/posts/1
-			wrapperDataRequest.innerHTML = this.dataRequest.title;
-
-			var labelDataRequest = document.createElement("label");
-			// Use translate function
-			//             this id defined in translations files
-			labelDataRequest.innerHTML = this.translate("TITLE");
-
-
-			wrapper.appendChild(labelDataRequest);
-			wrapper.appendChild(wrapperDataRequest);
-		}
-
-		// Data from helper
-		if (this.dataNotification) {
-			var wrapperDataNotification = document.createElement("div");
-			// translations  + datanotification
-			wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
-
-			wrapper.appendChild(wrapperDataNotification);
-		}
-		return wrapper;
-	},
-
-	getScripts: function() {
-		return [];
-	},
-
-	getStyles: function () {
-		return [
-			"MMM-Scratch.css",
-		];
-	},
-
-	// Load translations files
-	getTranslations: function() {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			es: "translations/es.json",
-			ko: "translations/ko.json"
-		};
-	},
-
-	processData: function(data) {
-		var self = this;
-		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
-		this.loaded = true;
-
-		// the data if load
-		// send notification to helper
-		this.sendSocketNotification("MMM-Scratch-NOTIFICATION_TEST", data);
-	},
-
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MMM-Scratch-NOTIFICATION_TEST") {
-			// set dataNotification
-			this.dataNotification = payload;
-			this.updateDom();
-		}
 	},
 });
